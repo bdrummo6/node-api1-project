21c9d57853cd023a259bf032bba170839b0e0690
bdrummo6 <bradley.drummonds@gmail.com>
2020-06-13 16:07:33 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require("express")
-const shortid = require("shortid")
 
 const server = express()
 
@@ -7,17 +6,18 @@ let users = []
 
 server.use(express.json())
 
-const PORT = 5000
+const PORT = 8000 
+
 server.listen(PORT, () => 
-console.log(`\n ** API running on http://localhost:${PORT} **\n`)
+    console.log(`\n ** API running on http://localhost:${PORT} **\n`)
 )
 
-server.post("/api/users", (req, res) => {
+server.post("/users", (req, res) => {
     const usersInfo = req.body
     usersInfo.id = shortid.generate()
 
     if (!usersInfo.name  || !usersInfo.bio ){
-    res.status(400).json({errorMessage: "Please provide name and bio for the user."})
+		res.status(400).json({errorMessage: "Please provide name and bio for the user."})
     }
 
     else {
@@ -26,14 +26,13 @@ server.post("/api/users", (req, res) => {
     }
 })
 
-server.get("/api/users", (req, res) => {
+server.get("/users", (req, res) => {
     
-  
         res.status(201).json(users)
     
 })
 
-server.get('/api/users/:id', (req, res) => {
+server.get('/users/:id', (req, res) => {
     res.status(200).json({users: "get user by id successful!"})
 
     res.status(404).json({message: "The user with the specified ID does not exist"})
@@ -41,7 +40,7 @@ server.get('/api/users/:id', (req, res) => {
     res.status(500).json({errorMessage: "The user information could not be retrieved."})
 })
 
-server.delete('/api/users/:id', (req, res) => {
+server.delete('/users/:id', (req, res) => {
     res.status(200).json({users: "delete user by id successful!"})
 
     res.status(404).json({message: "The user with the specified ID does not exist."})
@@ -49,7 +48,7 @@ server.delete('/api/users/:id', (req, res) => {
     res.status(500).json({errorMessage: "The user could not be removed"})
 })
 
-server.patch('/api/users/:id', (req, res) => {
+server.patch('/users/:id', (req, res) => {
     res.status(200).json({users: "successful"})
 
     res.status(404).json({message: "The user with the specified ID foes not exist"})
